74cede786113dd2f64b00bc24cedd920453c0046
samarth <45701193+samarthgca@users.noreply.github.com>
2021-04-09 19:53:08 -0700

    Update main.c

--- main.c
+++ main.c
@@ -31,14 +31,14 @@ void loop()  {
       }
       Serial.print("         pm2.5: ");
       Serial.print(n2);
-       if (n2 < 300 && n2 > 200){
+       if (n2 < 500 && n2 > 100){
        digitalWrite(yellow, HIGH);
        delay(520);
        digitalWrite(yellow, LOW);
       }
       Serial.print("         pm10: ");
       Serial.print(n3);
-       if(n2 > 400){
+       if(n2 > 600){
        digitalWrite(red, HIGH);
        delay(520);
        digitalWrite(red, LOW);
